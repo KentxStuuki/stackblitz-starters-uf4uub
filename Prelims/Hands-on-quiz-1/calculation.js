5d9355626ff34dfb5c1d760c6147cae7dd0e7cba
KentxStuuki <mequiambao@outlook.com>
2024-07-30 21:39:39 +0800

    Commit to hands on quiz

---
+++ Prelims/Hands-on-quiz-1/calculation.js
@@ -0,0 +1,23 @@
+// Quiambao, Mark Erolle D.
+// WD-303
+
+module.exports = function(income, tax, sss, pagIbig, philHealth) {
+    var income = 7200;
+    var tax = 720;
+    var sss = 1200;
+    var pagIbig = 300;
+    var philHealth = 400;
+
+
+    this.income = income;
+    this.tax = tax;
+    this.sss = sss;
+    this.pagIbig = pagIbig;
+    this.philHealth = philHealth;
+    this.total = function() {
+        return this.tax + this.sss + this.pagIbig + this.philHealth;
+    }
+    this.deduction = function() {
+        return this.income - this.total();
+    }
+}
